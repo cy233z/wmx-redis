f1dd4a5dcc6b13e05d6e0927f6fe59de01ce6510
wangmaoxiong <2268461750@qq.com>
2020-09-15 19:25:38 +0800

    初始化

---
+++ src/main/java/com/wmx/wmxredis/WmxRedisApplication.java
@@ -0,0 +1,13 @@
+package com.wmx.wmxredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmxRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmxRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmx/wmxredis/wmx/Wangmx.java
@@ -0,0 +1,13 @@
+package com.wmx.wmxredis.wmx;
+
+/**
+ * @author wangMaoXiong
+ * @version 1.0
+ * @date 2020/9/12 9:24
+ */
+public class Wangmx {
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
