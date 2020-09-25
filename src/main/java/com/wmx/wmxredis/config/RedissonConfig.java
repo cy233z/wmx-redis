b22487ebee7f41b5ab2fdc605876712199922fcc
wangmaoxiong <2268461750@qq.com>
2020-09-25 20:10:44 +0800

    Redisson 分布式锁

--- src/main/java/com/wmx/wmxredis/config/RedissonConfig.java
+++ src/main/java/com/wmx/wmxredis/config/RedissonConfig.java
@@ -3,8 +3,6 @@ package com.wmx.wmxredis.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,32 +16,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
-    @Autowired
-    private RedisProperties properties;
-
     /**
-     * redis 服务器单机部署时，创建 RedissonClient 实例。
+     * redis 服务器单机部署时，创建 RedissonClient 实例，交由 Spring 容器管理
      *
      * @return
      */
     @Bean
     public RedissonClient redissonClient() {
-
-        System.out.println(properties);
-        System.out.println(properties.getDatabase());
-
         /**
-         * Config：Redisson 配置类
-         * SingleServerConfig useSingleServer()：初始化 redis 单服务器配置。即 redis 服务器单机部署
-         * setAddress(String address)：设置 redis 服务器地址。格式 -- 主机:端口
+         * Config：Redisson 配置基类，SingleServerConfig：单机部署配置类，MasterSlaveServersConfig：主从复制部署配置
+         * SentinelServersConfig：哨兵模式配置，ClusterServersConfig：集群部署配置类。
+         * useSingleServer()：初始化 redis 单服务器配置。即 redis 服务器单机部署
+         * setAddress(String address)：设置 redis 服务器地址。格式 -- 主机:端口，不写时，默认为 127.0.0.1:6379
+         * setDatabase(int database): 设置连接的 redis 数据库，默认为 0
+         * setPassword(String password)：设置 redis 服务器认证密码，没有时设置为 null，默认为 null
          * RedissonClient create(Config config): 使用提供的配置创建同步/异步 Redisson 实例
-         * RedissonClient create()：默认连接本地 127.0.0.1:6379
-         * Redisson 类实现了 RedissonClient 接口
+         * Redisson 类实现了 RedissonClient 接口，真正需要使用的就是这两个 API
          */
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        config.useSingleServer()
+                .setAddress("redis://127.0.0.1:6379")
+                .setDatabase(2)
+                .setPassword(null);
+
         RedissonClient redissonClient = Redisson.create(config);
         return redissonClient;
     }
-
 }
--- src/main/java/com/wmx/wmxredis/controller/RedissonController.java
+++ src/main/java/com/wmx/wmxredis/controller/RedissonController.java
@@ -26,31 +26,67 @@ public class RedissonController {
     private RedissonClient redissonClient;
 
     /**
-     * 支付：http://localhost:8080/redisson/payment?orderNumber=885867878
+     * 未加锁做任何限制时
+     * <p>
+     * 支付：http://localhost:8080/redisson/payment1?orderNumber=885867878
      *
      * @param orderNumber ：订单号
      * @return
      */
-    @GetMapping("redisson/payment")
-    public String payment(@RequestParam Integer orderNumber) {
-        String result = "支付成功!";
-        String key = "com.wmx.wmxredis.controller.RedissonController.payment_" + orderNumber;
-        RLock lock = redissonClient.getLock(key);
-        lock.lock();
+    @GetMapping("redisson/payment1")
+    public String payment1(@RequestParam Integer orderNumber) {
+        String result = "订单【" + orderNumber + "】支付成功!";
+        logger.info("用户请求支付订单【" + orderNumber + "】");
         try {
-            logger.info(": 查询支付人账户余额.");
-            TimeUnit.SECONDS.sleep(2);
-            logger.info(": 查询收款人账户状态.");
-            TimeUnit.SECONDS.sleep(2);
-            logger.info(": 向收款人支付人民币=" + orderNumber);
-            TimeUnit.SECONDS.sleep(2);
+            logger.info(": 查询支付状态.");
+            TimeUnit.SECONDS.sleep(4);
+            logger.info(": 开始支付订单【" + orderNumber + "】");
+            TimeUnit.SECONDS.sleep(4);
         } catch (Exception e) {
             e.printStackTrace();
-            result = "支付失败：" + e.getMessage();
+            result = "订单【" + orderNumber + "】支付失败：" + e.getMessage();
         } finally {
-            lock.unlock();
+            logger.info("结束支付订单【" + orderNumber + "】");
         }
         return result;
     }
 
+    /**
+     * 使用 Redisson 加锁 · 可重入锁
+     * <p>
+     * 支付：http://localhost:8080/redisson/payment2?orderNumber=885867878
+     *
+     * @param orderNumber ：订单号
+     * @return
+     */
+    @GetMapping("redisson/payment2")
+    public String payment(@RequestParam Integer orderNumber) {
+        String result = "订单【" + orderNumber + "】支付成功!";
+        logger.info("用户请求支付订单【" + orderNumber + "】");
+
+        String key = "com.wmx.wmxredis.controller.RedissonController.payment_" + orderNumber;
+        /**
+         * getLock(String name)：按名称返回锁实例，实现了一个非公平的可重入锁，因此不能保证线程获得顺序
+         * lock():获取锁，如果锁不可用，则当前线程将处于休眠状态，直到获得锁为止
+         */
+        RLock lock = redissonClient.getLock(key);
+        lock.lock();
+        try {
+            logger.info("查询支付状态.");
+            TimeUnit.SECONDS.sleep(4);
+            logger.info("开始支付订单【" + orderNumber + "】");
+            TimeUnit.SECONDS.sleep(4);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "订单【" + orderNumber + "】支付失败：" + e.getMessage();
+        } finally {
+            logger.info("结束支付订单【" + orderNumber + "】");
+            /**
+             * unlock()：释放锁， Lock 接口的实现类通常会对线程释放锁（通常只有锁的持有者才能释放锁）施加限制，
+             * 如果违反了限制，则可能会抛出（未检查的）异常。
+             */
+            lock.unlock();
+        }
+        return result;
+    }
 }
