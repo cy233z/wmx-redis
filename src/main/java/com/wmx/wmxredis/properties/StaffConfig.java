65ee9d1f165be529193c7bd9a0807ddbe43c78f1
wangmaoxiong <2268461750@qq.com>
2020-09-25 11:27:37 +0800

    @ConfigurationProperties + @EnableConfigurationProperties 取值

--- src/main/java/com/wmx/wmxredis/properties/StaffConfig.java
+++ src/main/java/com/wmx/wmxredis/properties/StaffConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 配置类
+ * EnableConfigurationProperties：将目标 java bean 配置类(ConfigurationProperties) 交由 Spring 容器管理
+ * 而后可以在任意地方使用 取值使用.
  *
  * @author wangMaoXiong
  * @version 1.0
