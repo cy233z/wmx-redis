be8597263cd5d97d554dc2c8dd7747357ed69f18
wangmaoxiong <2268461750@qq.com>
2021-12-25 18:59:11 +0800

    Redssion 操作 redis 5 种基本数据类型

--- src/main/java/com/wmx/wmxredis/redisson/RedssionHelloController.java
+++ src/main/java/com/wmx/wmxredis/redisson/RedssionHelloController.java
@@ -1,23 +1,30 @@
 package com.wmx.wmxredis.redisson;
 
 import cn.hutool.core.map.MapUtil;
-import org.redisson.api.RBucket;
-import org.redisson.api.RFuture;
-import org.redisson.api.RedissonClient;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.redisson.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Redssion 操作 redis 5 种基本数据类型
  * <p>
- * RBucket：对象处理器，最大大小支持 512MB，其中所有的操作都提供了同步和异步两种方法
+ * {@link RBucket}：对象处理器，可以操作字符串类型，最大大小支持 512MB，其中所有的操作都提供了同步和异步两种方法
+ * {@link RMap}：操作 Map 数据类型，继承自 ConcurrentMap，线程安全，key-value 都不允许为 null。同样有异步方法。
+ * {@link RList}：操作 List 列表数据类型
+ * {@link RSet}：操作 set 无序集合数据类型
+ * {@link RSortedSet}：操作有序集合数据类型
  *
  * @author wangMaoXiong
  * @version 1.0
@@ -33,11 +40,10 @@ public class RedssionHelloController {
     static long i = 20;
     static long sum = 300;
 
-
     // ========================== String =======================
 
     /**
-     * http://localhost:8080/redssion/hello/string/set/redssionS
+     * http://localhost:8080/redssion/hello/string/redssionS
      * <p>
      * RBucket<V> getBucket(String name)：按名称返回对象持有者实例。
      * <p>
@@ -57,8 +63,8 @@ public class RedssionHelloController {
      * @param key
      * @return
      */
-    @GetMapping("/string/set/{key}")
-    public Map<String, String> s1(@PathVariable String key) throws ExecutionException, InterruptedException {
+    @GetMapping("/string/{key}")
+    public Map<String, String> string(@PathVariable String key) throws ExecutionException, InterruptedException {
         //获取指定 key 的对象处理器, 字符串类型
         RBucket<String> stringRBucket = redissonClient.getBucket(key);
         //将元素同步持久化到库中.
@@ -85,12 +91,132 @@ public class RedssionHelloController {
         return map;
     }
 
-    @GetMapping("/get/{key}")
-    public String g1(@PathVariable String key) {
-// 设置字符串
-        RBucket<String> keyObj = redissonClient.getBucket(key);
-        String s = keyObj.get();
-        return s;
+    // ========================== Map =======================
+
+    /**
+     * http://localhost:8080/redssion/hello/map/redssionM
+     * <p>
+     * RMap<K, V> getMap(String name)：通过 key 获取 map 对象，可用于操作 map 数据类型。
+     * <p>
+     * V put(K key, V value)：往 map 对象中添加元素，持久化到库中.
+     * void putAll(java.util.Map<? extends K, ? extends V> map)：往 map 对象中添加元素，持久化到库中.
+     * void putAll(Map<? extends K, ? extends V> map, int batchSize)：往 map 对象中添加元素，持久化到库中，指定每个批次添加的元素个数.
+     * V putIfAbsent(K key, V value)：map 中的不存在 key 时才进行赋值。
+     * <p>
+     * boolean expire(long timeToLive, TimeUnit timeUnit): 设置 key 的过期时间，在持久化到库中后再设置过期时间
+     * <p>
+     * Set<Entry<K, V>> readAllEntrySet()：一次读取 map 中的所有键值对
+     * Set<K> readAllKeySet()：一次读取 map 中的所有 key
+     * Collection<V> readAllValues()：一次读取 map 中的所有 value
+     * Map<K, V> readAllMap(): 以本地实例一次读取所有元素。
+     * Set<java.util.Map.Entry<K, V>> entrySet()：获取 Map 中的元素集合，以批次获取，每批次获取10个。
+     * V get(Object key)：获取元素
+     * Map<K, V> getAll(Set<K> keys)：获取元素
+     *
+     * @param key
+     * @return
+     */
+    @GetMapping("/map/{key}")
+    public Map<String, Object> map(@PathVariable String key) {
+
+        // 获取处理 map 数据类型的对象，往对象中添加元素时，就会持久化到库中
+        RMap<String, Object> rMap = redissonClient.getMap("RMap");
+
+        rMap.put(key, key + "-V2");
+
+        Map<String, String> stringMap = MapUtil.builder("id", "P988").put("code", "200").put("name", "张三").map();
+        rMap.putAll(stringMap);
+
+        //ifAbsent 不存在，所以1会设置成功，2不会再设置
+        rMap.putIfAbsent("ifAbsent", 1);
+        rMap.putIfAbsent("ifAbsent", 2);
+
+        //遍历所有元素
+        for (Map.Entry<String, Object> entry : rMap.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+
+        //设置超时时间，在持久化到库中后在设置过期时间
+        boolean expire = rMap.expire(1 * 60, TimeUnit.SECONDS);
+        System.out.println("expire=" + expire);
+
+        return rMap;
+    }
+
+    // ========================== List =======================
+
+    /**
+     * http://localhost:8080/redssion/hello/list/redssionL
+     * <p>
+     * int addAfter(V elementToFind, V element)：将新元素添加到第一个元素(elementToFind)的后面
+     * int addBefore(V elementToFind, V element)：将新元素添加到第一个元素(elementToFind)的前面
+     * <p>
+     * List<V> readAll()：一次读取所有元素
+     *
+     * @param key
+     * @return
+     */
+    @GetMapping("/list/{key}")
+    public List<Object> list(@PathVariable String key) {
+        RList<Object> rList = redissonClient.getList(key);
+        rList.add(1);
+        rList.addAll(Lists.newArrayList(2, 3, 4, 5, 3, 7, 8, 9));
+
+        rList.addAfter(3, "三");
+        rList.addBefore(3, "叁");
+
+        List<Object> readAll = rList.readAll();
+        System.out.println("readAll=" + readAll);
+
+        rList.expire(3 * 60, TimeUnit.SECONDS);
+        return rList;
+    }
+
+    // ========================== Set =======================
+
+    /**
+     * http://localhost:8080/redssion/hello/set/RSet
+     *
+     * @param key
+     * @return
+     */
+    @GetMapping("/set/{key}")
+    public Set<Object> set(@PathVariable String key) {
+        RSet<Object> rSet = redissonClient.getSet(key);
+        rSet.add("a");
+
+        rSet.addAll(Sets.newHashSet("b", "c"));
+        rSet.addAll(Sets.newHashSet(2, 34, 5, 67, 35, 78, 87));
+
+        Set<Object> readAll = rSet.readAll();
+        System.out.println("readAll=" + readAll);
+
+        rSet.expire(3 * 60, TimeUnit.SECONDS);
+        return rSet;
+    }
+
+    // ========================== SortedSet =======================
+
+    /**
+     * http://localhost:8080/redssion/hello/sortedSet/RSortedSet
+     *
+     * @param key
+     * @return
+     */
+    @GetMapping("/sortedSet/{key}")
+    public Set sortedSet(@PathVariable String key) {
+        RSortedSet<Integer> rSortedSet = redissonClient.getSortedSet(key);
+
+        boolean add = rSortedSet.add(88);
+        System.out.println(add);
+
+        rSortedSet.addAll(Sets.newHashSet(2, 3, 4, 5, 6, 7, 8, 9));
+
+        Collection<Integer> readAll = rSortedSet.readAll();
+        //readAll=[2, 3, 4, 5, 6, 7, 8, 9, 88]
+        System.out.println("readAll=" + readAll);
+
+        return rSortedSet;
     }
 
 }
