750478ee62f2a2be4c479514643a29955b03ab74
wangmaoxiong <2268461750@qq.com>
2021-12-29 19:10:26 +0800

    Spring Boot + hibernate-validator 实现各种 web 参数校验

--- src/main/java/com/wmx/wmxredis/validator/ValidatorConfig.java
+++ src/main/java/com/wmx/wmxredis/validator/ValidatorConfig.java
@@ -9,9 +9,6 @@ import javax.validation.ValidatorFactory;
 
 
 /**
- * 自定义 javax.validation.Validator 配置，覆盖默认值。
- * 1、无论是对 @Validated 注解、还是编程式校验都有效。
- *
  * @author wangMaoXiong
  * @version 1.0
  * @date 2021/12/29 18:50
@@ -19,6 +16,12 @@ import javax.validation.ValidatorFactory;
 @Configuration
 public class ValidatorConfig {
 
+    /**
+     * 自定义 javax.validation.Validator 配置，覆盖默认值。
+     * 1、无论是对 @Validated 注解、还是编程式校验都有效。
+     *
+     * @return
+     */
     @Bean
     public javax.validation.Validator validator() {
         ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
