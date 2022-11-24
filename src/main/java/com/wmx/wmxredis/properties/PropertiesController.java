173302840d3a90d5655f07f91e421afdb72e7c15
wangmaoxiong <2268461750@qq.com>
2022-11-24 19:46:35 +0800

    Redis 5种数据类型测试

--- src/main/java/com/wmx/wmxredis/properties/PropertiesController.java
+++ src/main/java/com/wmx/wmxredis/properties/PropertiesController.java
@@ -1,6 +1,5 @@
 package com.wmx.wmxredis.properties;
 
-import com.wmx.wmxredis.annotation.RedisLock;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +35,6 @@ public class PropertiesController {
      * @return
      */
     @GetMapping("properties/getDefaultUser")
-    @RedisLock(retryCount = 3, lockTime = 30, lockFiled = -1, desc = "redis分布式锁")
     public UserProperties getDefaultUser() {
         return userProperties;
     }
